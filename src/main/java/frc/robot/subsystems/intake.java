0aa9fd7deebc8c68f06d81b7e587b47818e11d26
BobdxProgrammer <116928333+BobdxProgrammer@users.noreply.github.com>
2024-03-15 17:40:59 -0400

    Update intake.java

--- src/main/java/frc/robot/subsystems/intake.java
+++ src/main/java/frc/robot/subsystems/intake.java
@@ -1,20 +1,43 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.SparkMaxRelativeEncoder;
 
-import static frc.robot.Constants.IntakeConstants.kIntake;
+import static frc.robot.Constants.IntakeConstants.kIntake_motor1ID;
+import static frc.robot.Constants.IntakeConstants.kIntake_motor2ID;
+import static frc.robot.Constants.IntakeConstants.kIntakeSpeed;
+
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class intake extends SubsystemBase{
-    //CANSparkMax Ryan
+    CANSparkMax motor1, motor2;
     public intake(){
-    //    Ryan = new CANSparkMax(kIntake, MotorType.kBrushed);
+        motor1 = new CANSparkMax(kIntake_motor1ID, MotorType.kBrushed);
+        motor2 = new CANSparkMax(kIntake_motor2ID, MotorType.kBrushed);
+        motor1.follow(motor2);
+        RelativeEncoder m_motor1 = motor1.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);//changable
+        RelativeEncoder m_motor2 = motor2.getEncoder(SparkMaxRelativeEncoder.Type.kHallSensor, 42);//changable
     }
-    /*public Command down(){
-
+    public Command down(){
+        return startEnd(
+        // When the command is initialized, set the wheels to the intake speed values
+        () -> {
+            set_speed(kIntakeSpeed);        },
+        // When the command stops, stop the wheels
+        () -> {
+          stop();
+        });
+    }
+    public void set_speed(double speed){
+        motor1.set(speed);
+    }
+    public void stop(){
+        motor1.set(0);
     }
     //public //https://docs.revrobotics.com/through-bore-encoder/specifications, to view the number of rotations, find example code for reference
     //2048 rev
-    */
+    
 }
